e29de4913d7966d277e2d262ef6c266688b290e3
prasanth7499 <47467146+prasanth7499@users.noreply.github.com>
2019-02-09 16:18:10 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	int s;
+	scanf("%d",&s);
+	if (s>0)
+	{
+		printf("\n positive");
+	
+	}
+	else if (s<0)
+	{
+		printf("\n negative");
+	}
+	else
+	{
+		printf("\n zero");
+	}
+	
+	return 0;
+}
